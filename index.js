8be3e9f48a6564556aa3ee6bcf43ae47beae3cde
Jaden Wang <jadenyjw@gmail.com>
2019-02-05 20:13:38 -0500

    Bot lists users and leaves on song end

--- index.js
+++ index.js
@@ -16,17 +16,34 @@ client.on('message', message => {
   if (tokenized[0] === '/karaoke') {
    // Only try to join the sender's voice channel if they are in one themselves
    if (message.member.voiceChannel) {
-     message.member.voiceChannel.join()
+
+     const vc = message.member.voiceChannel;
+
+     vc.join()
        .then(connection => { // Connection is an instance of VoiceConnection
          message.reply('now playing requested song.');
+
+         map = {};
+
          const receiver = connection.createReceiver();
+
+         vc.members.forEach(function(guildMember, guildMemberId) {
+            if(guildMember.user != client.user){
+              console.log(guildMemberId, guildMember.user.username);
+            }
+         });
+
          const stream = ytdl(url, { filter : 'audioonly' });
-         const dispatcher = connection.playStream(stream);
+
+         const dispatcher = connection.playStream(stream).on("end",()=>{
+                vc.leave()
+          });
        })
        .catch(console.log);
+
        } else {
-     message.reply('You need to join a voice channel first!');
-   }
+         message.reply('You need to join a voice channel first!');
+       }
  }
 
 });
